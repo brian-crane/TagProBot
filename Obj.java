f068ee33cf58202f25bf7cc8fad851213fd63ee5
MrGundBear <briancrane@gmail.com>
2014-07-05 16:36:28 -0600

    Create Obj.java

---
+++ Obj.java
@@ -0,0 +1,19 @@
+
+public class Obj
+{
+	int x,y, type;
+	boolean mark = false;
+	
+	public Obj(int x, int y, int t)
+	{
+		mark = false;
+		//0blue,1red,2obstacle,3mainguyblue,4mainguyred,5spikesthis is one of the be
+		this.x = x;
+		this.y = y+32;
+		type = t;
+		
+	}
+	
+	
+
+}
